964bfacada1f978eedfd4bac1370e03d9476c5d4
zhiqixie9-jpg <zhiqixie9@gmail.com>
2025-12-10 21:25:47 +0800

    Remove commented relationship section in favorite model

--- backend/app/models/favorite.py
+++ backend/app/models/favorite.py
@@ -8,6 +8,6 @@ class Favorite(Base):
     user_id = Column(Integer, ForeignKey("User.user_id"), primary_key=True)
     product_id = Column(Integer, ForeignKey("Product.product_id"), primary_key=True)
 
-    # 关系
+
     user = relationship("User", back_populates="favorites")
     product = relationship("Product", back_populates="favorites")
