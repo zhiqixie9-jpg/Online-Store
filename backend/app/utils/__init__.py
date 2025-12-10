149134ce46e0e16432de669d3cd82caf258b6542
zhiqixie9-jpg <zhiqixie9@gmail.com>
2025-12-10 20:47:39 +0800

    Clean up comments in utils init file
    Removed comments about importing and exporting utility functions.

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,4 +1,4 @@
-# 显式导入所有工具函数
+
 from .auth_utils import (
     verify_password,
     get_password_hash,
@@ -7,7 +7,7 @@ from .auth_utils import (
 )
 from .member_utils import update_member_status
 
-# 导出所有工具函数
+
 __all__ = [
     "verify_password",
     "get_password_hash", 
